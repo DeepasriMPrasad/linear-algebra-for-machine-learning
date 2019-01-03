c81a906317d2b22bd11a11c71fe4d870566f96f2
Ravi Kalia <ravkalia@gmail.com>
2019-01-03 17:20:13 -0600

    clean up

--- solving_simultaneous_eqns.py
+++ solving_simultaneous_eqns.py
@@ -1,8 +1,8 @@
 import numpy as np
 
-'''For general matrix A nxp, vectors b and x (nx1), representing a linear system of equations with 
-x unknown, the solution can be found by using np.linalg.solve, which decides on the best way to invert 
-the matrix (this depends on size, stability, sparsity and other properties of A) 
+'''For general matrix A nxp, vectors b and x (nx1), representing a linear system of equations with
+x unknown, the solution can be found by using np.linalg.solve, which decides on the best way to invert
+the matrix (this depends on size, stability, sparsity and other properties of A)
 and solve the simultaneous equations
 Ax = b
 '''
@@ -11,12 +11,10 @@ A = np.array([3, 2, 2, 3]).reshape((2, 2))
 b = np.array([7, 8]).reshape((2, 1))
 x = np.linalg.solve(A, b)
 
-print(x)
-
 
 def SimSolver(dataA, shapeA, dataB, shapeB):
     """
-    Simultaneous equation solver, using internal 
+    Simultaneous equation solver, using internal
     numpy linear algebra routines to solve the matrix inversion
     via most appropriate factorization
     """
@@ -24,3 +22,6 @@ def SimSolver(dataA, shapeA, dataB, shapeB):
     b = np.array(dataB).reshape(shapeB)
     x = np.linalg.solve(A, b)
     return x
+
+
+print(SimSolver([3, 2, 2, 3], (2, 2), [7, 8], (2, 1)) == x)
