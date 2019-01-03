c18ac61bc29f695470b270476b3e7090d84dbc6f
Ravi Kalia <ravkalia@gmail.com>
2019-01-03 17:07:01 -0600

    extract function into docstring function

--- solving_simultaneous_eqns.py
+++ solving_simultaneous_eqns.py
@@ -10,4 +10,17 @@ Ax = b
 A = np.array([3, 2, 2, 3]).reshape((2, 2))
 b = np.array([7, 8]).reshape((2, 1))
 x = np.linalg.solve(A, b)
+
 print(x)
+
+
+def SimSolver(dataA, shapeA, dataB, shapeB):
+    """
+    Simultaneous equation solver, using internal 
+    numpy linear algebra routines to solve the matrix inversion
+    via most appropriate factorization
+    """
+    A = np.array(dataA).reshape(shapeA)
+    b = np.array(dataB).reshape(shapeB)
+    x = np.linalg.solve(A, b)
+    return x
